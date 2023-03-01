79170e6274e72e354111cc79b23f3fc0167546b8
Nikhil verma <nv24805@gmail.com>
2023-03-01 21:40:42 +0530

    First

---
+++ Nikhil_NotePad (Project 2)/src/main/java/com/mycompany/nikhil_notepad/Nikhil_NotePad.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nikhil_notepad;
+/**
+ *
+ * @author Deepanjali Soni
+ */
+public class Nikhil_NotePad {
+
+    public static void main(String[] args) {
+         MyApp notepad = new MyApp();
+    }
+}
